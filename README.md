3f7cce186ebe6b0bc8a3eee1aafa267e620d04a1
trakkyo <primrose_aquasheep@yahoo.co.jp>
2020-03-04 09:57:33 +0900

    コード作成

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# split_image
-画像ペアの振り分け
+# 学習用画像ペア振り分けツール
+
+## 事前インストール
+
+```
+pip install -r requirements.txt
+```
+* python3環境が設定済みである前提
+
+## 手順
+
+- 画像の配置
+  - 配置先
+    - static/train_A: 元画像
+    - static/train_B: GroundTruth
+  - 元画像とGroundTruthは同一ファイル名ペアにする
+    - ファイル名を「6桁数値.jpg(例: 000000.jpg)」とし、連番で配置する
+
+- スクリプト実行
+  ```
+  python app.py
+  ```
+
+- 画像振り分け作業
+  - ブラウザで「http://localhost:8888」でアクセス
+  - 以下の画面が表示される
+    ![flower](https://user-images.githubusercontent.com/14009787/75833590-d7b00400-5dfc-11ea-9475-21493d87e3d2.png)
+
+- 結果
+  - 「Use img」押下でsave配下に画像をコピーして次の画像へ
+  - 「don't use」押下で対象画像のコピーをスキップして次の画像へ
+  - 「Finished」の表示で完了
+
+## その他
+
+- 画像の削除考慮なし
+  - コピー対象でないものをコピーしたら手動削除の必要あり
+- 画像番号の指定機能なし
+
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import os, sys, logging, urllib, shutil
+
+
+app = Flask(__name__)
+
+logging.basicConfig(filename='./logger.log', level=logging.DEBUG)
+
+def _make_file_path(index):
+    imgfilelist = []
+    traintype = []
+    relativepath = 'static/'
+    filename = '{:0=6}'.format(index)
+
+    for train in ['train_A', 'train_B']:
+        filepath = os.path.join(relativepath, train, filename + '.jpg')
+        if os.path.isfile(filepath):
+            imgfilelist.append(os.path.join(relativepath, train, filename + '.jpg'))
+            traintype.append(train)
+
+    return imgfilelist, traintype
+
+@app.route("/")
+def root():
+    count = 0
+    imgfilelist, _ = _make_file_path(count)
+    return render_template("index.html", imgfilelist = imgfilelist, count = count)
+
+@app.route("/next")
+def test():
+    res = urllib.parse.unquote(request.args.get('use_img'))
+    type, count = res.split(',')
+
+    imgfilelist, traintype = _make_file_path(int(count))
+    if type == '1':
+        # save file
+        savepath = 'save/'
+        for imgfile, type in zip(imgfilelist, traintype):
+            shutil.copy(imgfile, os.path.join('savepath', type))
+
+    imgfilelist, traintype = _make_file_path(int(count) + 1)
+    if len(imgfilelist) == 0:
+        return 'Finished'
+
+    return render_template("index.html", imgfilelist = imgfilelist, count = int(count) + 1)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8888)
